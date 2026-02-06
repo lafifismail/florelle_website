11b9f402717def3d468cd6a06491ad758a3997fe
lafifismail <lafifismail16@gmail.com>
2026-02-06 19:03:18 +0100

    fix(mail): add tls bypass for IP-based SMTP connection

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,6 +4,9 @@ const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: false, // true for 465, false for other ports
+    tls: {
+        rejectUnauthorized: false
+    },
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
